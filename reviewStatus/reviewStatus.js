283c72ea7a0e6664b415d9028d9395eafe4966f7
danieladamharding <danieladamharding@gmail.com>
2017-05-30 13:38:04 +0100

    Review status

---
+++ reviewStatus/reviewStatus.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">/*<![CDATA[*/
+    /* Insert CSS to style search input */
+    var link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", "https://staff.keele.ac.uk/bbcswebdav/xid-2827583_1");
+    var head = document.getElementsByTagName("head") [0];
+    head.appendChild(link);
+    /* Insert Font Awesome for search icon */
+    var link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+    var head = document.getElementsByTagName("head") [0];
+    head.appendChild(link);
+/*]]>*/</script>
